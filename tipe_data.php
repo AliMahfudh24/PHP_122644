0d909ca3f612374beee6bc6f46f88e1cc34d1e77
AliMahfudh24 <alimahfudhkurniawan@gmail.com>
2015-04-28 14:12:18 +0700

    Tugas Pemrograman Web

---
+++ tipe_data.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<?php
+class mobil {
+     function mobil() {
+         $this->model = "Hammer";
+     }
+}
+// create an object
+$Ironhide = new mobil();
+
+// show object properties
+echo $Ironhide->model;
+?>
+
+</body>
+</html>
